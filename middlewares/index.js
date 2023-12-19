3821e276d2df216bff33e2221f6e0d7c07a01575
Kristina Holub <christigolub@gmail.com>
2023-12-19 02:29:09 +0200

    code refactoring

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,9 @@
 const validateBody = require("./validateBody");
 const validateFavorite = require("./validateFavorite");
-const handleMongooseError = require("./handleMongooseError");
 const isValidId = require("./isValidId");
 
 module.exports = {
     validateBody,
     validateFavorite,
-    handleMongooseError,
     isValidId,
 };
